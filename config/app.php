22327ff62643a3f89e70c286cbe2512ade487caa
AungZinTun <42179343+AungZinTun@users.noreply.github.com>
2019-02-19 00:11:44 +0630

    dusk

--- config/app.php
+++ config/app.php
@@ -1,7 +1,5 @@
 <?php
-
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Application Name
@@ -12,9 +10,7 @@ return [
     | any other location as required by the application or its packages.
     |
     */
-
     'name' => 'TB',
-
     /*
     |--------------------------------------------------------------------------
     | Application Environment
@@ -25,9 +21,7 @@ return [
     | services the application utilizes. Set this in your ".env" file.
     |
     */
-
     'env' => env('APP_ENV', 'production'),
-
     /*
     |--------------------------------------------------------------------------
     | Application Debug Mode
@@ -38,9 +32,7 @@ return [
     | application. If disabled, a simple generic error page is shown.
     |
     */
-
     'debug' => env('APP_DEBUG', false),
-
     /*
     |--------------------------------------------------------------------------
     | Application URL
@@ -51,9 +43,7 @@ return [
     | your application so that it is used when running Artisan tasks.
     |
     */
-
     'url' => env('APP_URL', 'http://localhost'),
-
     /*
     |--------------------------------------------------------------------------
     | Application Timezone
@@ -64,9 +54,7 @@ return [
     | ahead and set this to a sensible default for you out of the box.
     |
     */
-
     'timezone' => 'UTC',
-
         /*
      |--------------------------------------------------------------------------
      | Application Date Format
@@ -76,13 +64,11 @@ return [
      | will be used with date and date-time functions.
      |
      */
-
     'date_format' => 'Y-m-d',
     'date_format_js' => 'yy-mm-dd',
     'date_format_moment' => 'YYYY-MM-DD',
     'time_format_moment' => 'HH:mm:ss',
     'datetime_format_moment' => 'YYYY-MM-DD HH:mm:ss',
-
     /*
     |--------------------------------------------------------------------------
     | Application Locale Configuration
@@ -93,9 +79,7 @@ return [
     | to any of the locales which will be supported by the application.
     |
     */
-
     'locale' => 'en',
-
     /*
     |--------------------------------------------------------------------------
     | Application Fallback Locale
@@ -106,9 +90,7 @@ return [
     | the language folders that are provided through your application.
     |
     */
-
     'fallback_locale' => 'en',
-
     /*
     |--------------------------------------------------------------------------
     | Encryption Key
@@ -119,11 +101,8 @@ return [
     | will not be safe. Please do this before deploying an application!
     |
     */
-
     'key' => env('APP_KEY'),
-
     'cipher' => 'AES-256-CBC',
-
     /*
     |--------------------------------------------------------------------------
     | Autoloaded Service Providers
@@ -134,9 +113,7 @@ return [
     | this array to grant expanded functionality to your applications.
     |
     */
-
     'providers' => [
-
         /*
          * Laravel Framework Service Providers...
          */
@@ -162,15 +139,11 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        
-
-
         /*
          * Package Service Providers...
          */
         Yajra\DataTables\DataTablesServiceProvider::class,
         
-
         /*
          * Application Service Providers...
          */
@@ -179,9 +152,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
     ],
-
     /*
     |--------------------------------------------------------------------------
     | Class Aliases
@@ -192,9 +163,7 @@ return [
     | the aliases are "lazy" loaded so they don't hinder performance.
     |
     */
-
     'aliases' => [
-
         'App' => Illuminate\Support\Facades\App::class,
         'Artisan' => Illuminate\Support\Facades\Artisan::class,
         'Auth' => Illuminate\Support\Facades\Auth::class,
@@ -229,13 +198,6 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         
-
-
-        'Form'      => Collective\Html\FormFacade::class,
-        'Html'      => Collective\Html\HtmlFacade::class,
-        'Flash'     => Laracasts\Flash\Flash::class,
     ],
-
     
-
 ];

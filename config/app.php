2b3e58c2ed674a6dc27cba04e56018c69d95514c
AungZinTun <42179343+AungZinTun@users.noreply.github.com>
2019-02-19 00:14:44 +0630

    dusk

--- config/app.php
+++ config/app.php
@@ -1,5 +1,7 @@
 <?php
+
 return [
+
     /*
     |--------------------------------------------------------------------------
     | Application Name
@@ -10,7 +12,9 @@ return [
     | any other location as required by the application or its packages.
     |
     */
+
     'name' => 'TB',
+
     /*
     |--------------------------------------------------------------------------
     | Application Environment
@@ -21,7 +25,9 @@ return [
     | services the application utilizes. Set this in your ".env" file.
     |
     */
+
     'env' => env('APP_ENV', 'production'),
+
     /*
     |--------------------------------------------------------------------------
     | Application Debug Mode
@@ -32,7 +38,9 @@ return [
     | application. If disabled, a simple generic error page is shown.
     |
     */
+
     'debug' => env('APP_DEBUG', false),
+
     /*
     |--------------------------------------------------------------------------
     | Application URL
@@ -43,7 +51,9 @@ return [
     | your application so that it is used when running Artisan tasks.
     |
     */
+
     'url' => env('APP_URL', 'http://localhost'),
+
     /*
     |--------------------------------------------------------------------------
     | Application Timezone
@@ -54,7 +64,9 @@ return [
     | ahead and set this to a sensible default for you out of the box.
     |
     */
+
     'timezone' => 'UTC',
+
         /*
      |--------------------------------------------------------------------------
      | Application Date Format
@@ -64,11 +76,13 @@ return [
      | will be used with date and date-time functions.
      |
      */
+
     'date_format' => 'Y-m-d',
     'date_format_js' => 'yy-mm-dd',
     'date_format_moment' => 'YYYY-MM-DD',
     'time_format_moment' => 'HH:mm:ss',
     'datetime_format_moment' => 'YYYY-MM-DD HH:mm:ss',
+
     /*
     |--------------------------------------------------------------------------
     | Application Locale Configuration
@@ -79,7 +93,9 @@ return [
     | to any of the locales which will be supported by the application.
     |
     */
+
     'locale' => 'en',
+
     /*
     |--------------------------------------------------------------------------
     | Application Fallback Locale
@@ -90,7 +106,9 @@ return [
     | the language folders that are provided through your application.
     |
     */
+
     'fallback_locale' => 'en',
+
     /*
     |--------------------------------------------------------------------------
     | Encryption Key
@@ -101,8 +119,11 @@ return [
     | will not be safe. Please do this before deploying an application!
     |
     */
+
     'key' => env('APP_KEY'),
+
     'cipher' => 'AES-256-CBC',
+
     /*
     |--------------------------------------------------------------------------
     | Autoloaded Service Providers
@@ -113,7 +134,9 @@ return [
     | this array to grant expanded functionality to your applications.
     |
     */
+
     'providers' => [
+
         /*
          * Laravel Framework Service Providers...
          */
@@ -139,11 +162,18 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        
+
+        Collective\Html\HtmlServiceProvider::class,
+Prettus\Repository\Providers\RepositoryServiceProvider::class,
+
+
         /*
          * Package Service Providers...
          */
         Yajra\DataTables\DataTablesServiceProvider::class,
         
+
         /*
          * Application Service Providers...
          */
@@ -152,7 +182,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
     ],
+
     /*
     |--------------------------------------------------------------------------
     | Class Aliases
@@ -163,7 +195,9 @@ return [
     | the aliases are "lazy" loaded so they don't hinder performance.
     |
     */
+
     'aliases' => [
+
         'App' => Illuminate\Support\Facades\App::class,
         'Artisan' => Illuminate\Support\Facades\Artisan::class,
         'Auth' => Illuminate\Support\Facades\Auth::class,
@@ -198,6 +232,13 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         
+
+
+        'Form'      => Collective\Html\FormFacade::class,
+        'Html'      => Collective\Html\HtmlFacade::class,
+        'Flash'     => Laracasts\Flash\Flash::class,
     ],
+
     
+
 ];

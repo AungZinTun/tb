607a354163346ba55e8546e7f2f7e61c081b255e
AungZinTun <42179343+AungZinTun@users.noreply.github.com>
2019-02-19 00:02:05 +0630

    dusk

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => 'TB',
+    'name' => 'Pmct',
 
     /*
     |--------------------------------------------------------------------------
@@ -162,6 +162,13 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        
+
+        Collective\Html\HtmlServiceProvider::class,
+Laracasts\Flash\FlashServiceProvider::class,
+Prettus\Repository\Providers\RepositoryServiceProvider::class,
+\InfyOm\Generator\InfyOmGeneratorServiceProvider::class,
+\InfyOm\AdminLTETemplates\AdminLTETemplatesServiceProvider::class, 
 
         /*
          * Package Service Providers...
@@ -227,6 +234,11 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         
+
+
+        'Form'      => Collective\Html\FormFacade::class,
+        'Html'      => Collective\Html\HtmlFacade::class,
+        'Flash'     => Laracasts\Flash\Flash::class,
     ],
 
     
